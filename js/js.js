db358c984da2bbdfb2faace60312e0b1c625cbb7
Sarasofia1214 <dasaro1206@gmail.com>
2025-05-26 08:11:08 -0500

    feat: :sparkles: Adding the styles

--- js/js.js
+++ js/js.js
@@ -1,4 +1,4 @@
-const api= "https://68335f6e464b499636ff3ec4.mockapi.io/prueba/filtro";
+const api= "https://www.themealdb.com/api/json/v1/1/search.php?f=b";
 const container = document.getElementById("container_comida");
 const plantilla = document.getElementById("template_comida");
 const more_info = document.getElementById("tarjeta_info")
@@ -6,7 +6,7 @@ const more_info = document.getElementById("tarjeta_info")
 fetch (api)
 .then (res => res.json())
 .then (data =>{
-    const  recetas= data[0].meals;
+    const  recetas= data.meals;
 
     recetas.forEach(receta => {
         const clone = plantilla.content.cloneNode(true);
@@ -28,26 +28,26 @@ fetch (api)
       });
     });
   
-  
-//   function mostrarInformacion(receta) {
-//     document.getElementById('foto_info').src = receta.foto || './img/default.png';
-//     document.getElementById('nombre_info').textContent = receta.nombre;
-//     document.getElementById('pais_info').textContent = receta.pais || 'Desconocido';
-//     document.getElementById('genero_info').textContent = receta.genero || 'Pop';
-  
-  
-//     if (modal.style.display === 'block') {
-//       modal.style.display = 'none';
-     
-//     } else {
-//       modal.style.display = 'block';
-  
-  
-//     }
-//   }
-  
-  
-//   document.getElementById('btn_cerrar').onclick = () => {
-//     modal.style.display = 'none';
-//   };
-  
+    function mostrarInformacion(receta) {
+        document.getElementById('foto_info').src = receta.strMealThumb || 'https://media.istockphoto.com/id/886600658/es/foto/plato-vac%C3%ADo-de-alimentos-despu%C3%A9s-de-comer-en-una-mesa-de-madera.jpg?s=170667a&w=0&k=20&c=awejW2hSeOCQd1EHcEr7VthUPhr5RmFF_9pOHSHzSKk=';
+        document.getElementById('nombre_info').textContent = receta.strMeal;
+        document.getElementById('instructions_info').textContent = receta.strInstructions || 'Desconocido';
+        document.getElementById('area_info').textContent = receta.strArea || 'Dessert';
+      
+      
+        if (more_info.style.display === 'block') {
+          more_info.style.display = 'none';
+         
+        } else {
+          more_info.style.display = 'block';
+        }
+      }
+      
+      
+      document.getElementById('btn_cerrar').onclick = () => {
+        modal.style.display = 'none';
+      };
+      
+      
+      
+      
